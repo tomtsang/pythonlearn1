ed14ff3930045566fb24750cd4a99535d99179e0
tom <273412935@qq.com>
2015-05-17 11:12:32 +0800

    add src/test/mytimer.py

---
+++ src/test/mytimer.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*- 
+'''
+Created on 2015Äê5ÔÂ17ÈÕ
+
+@author: linglong
+'''
+# File mytimer.py
+import time
+reps = 1000
+repslist = range(reps)
+def timer(func, *pargs, **kargs):
+    start = time.clock()
+    for i in repslist:
+        ret = func(*pargs, **kargs)
+    elapsed = time.clock() - start
+    return (elapsed, ret)
